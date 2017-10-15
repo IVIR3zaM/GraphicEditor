269d03f243a7fd5e37e7889ff061238560f9a3ce
Mohammad Reza Maghool <m.reza.maghool@gmail.com>
2017-10-15 05:38:13 +0330

    Basic setup files added

---
+++ tests/bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+
+if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
+    require_once(__DIR__ . '/../vendor/autoload.php');
+} elseif (file_exists(__DIR__ . '/../../../../vendor/autoload.php')) {
+    require_once(__DIR__ . '/../../../../vendor/autoload.php');
+} else {
+    throw new \Exception('autoload file not found');
+}
